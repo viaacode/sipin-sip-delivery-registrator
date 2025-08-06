6437d5d12b739b52955151cb2fe23e6d0cb0714e
Mattias.Poppe <poppe.mattias@gmail.com>
2025-08-06 15:48:08 +0200

    C02P20-18 Add basic event listener class

---
+++ app/app.py
@@ -0,0 +1,16 @@
+APP_NAME = "sipin-sip-delivery-registrator"
+
+
+class EventListener:
+    """EventListener is responsible for listening to Pulsar events and processing them."""
+
+    def __init__(self):
+        """Initializes the EventListener with configuration, logging, and Pulsar client."""
+        pass
+
+    def start_listening(self):
+        """
+        Starts listening for incoming messages from the Pulsar topic.
+        """
+        while True:
+            pass
